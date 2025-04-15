e49856164174e576ac4e7748b58e48348469e05e
Naveen pandian <npandian515@gmail.com>
2025-04-15 14:30:16 +0530

    started a trade bot like an d woow factors code init

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const cors = require('cors')
+const express = require('express')
+const app = express()
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req , res) => {
+    res.send("API gawk")
+
+});
+
+app.listen(5000 , () => {
+    console.log("Server is running on port 5000")
+})
